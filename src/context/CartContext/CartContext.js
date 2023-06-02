5f29c8b1c4060f6538eb295abe9480815df90d01
Nico Ramirez <ramireznicc.23@gmail.com>
2023-06-02 15:16:26 -0300

    fixing context bug

--- src/context/CartContext/CartContext.js
+++ src/context/CartContext/CartContext.js
@@ -1,11 +1,9 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 import { useCart } from "../../hooks/useCart";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const quantity = 12;
-
   return (
     <CartContext.Provider value={useCart()}>{children}</CartContext.Provider>
   );
